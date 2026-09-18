08576725d564d9ef8a7a48fdc604404ab19291f3
mrbharat007 <singhbharat7954@gmail.com>
2026-09-18 12:33:31 +0530

    Delete db/migrations directory

--- db/migrations/002_add_material_sector.sql
+++
@@ -1,5 +0,0 @@
--- Apply to an existing database (the base schema.sql only runs on first init).
---   docker compose exec -T db psql -U material_setu -d material_setu < db/migrations/002_add_material_sector.sql
-
-ALTER TABLE materials
-    ADD COLUMN IF NOT EXISTS sector TEXT;
